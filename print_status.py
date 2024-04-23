0d1c9ff5f3ff2a3e29855c98fe5a30bbb4d7012a
Deepkumar Pambhar <91485737+pambhar-deepkumar@users.noreply.github.com>
2024-04-23 09:04:14 +0200

    Update print_status.py

--- print_status.py
+++ print_status.py
@@ -1,12 +1,13 @@
 import random
 
 def main():
+    raise Exception("Raised")
     # List of possible outputs
     outputs = ["SUCCESS", "WARNING", "ERROR"]
     
     # Randomly choose one output
     selected_output = random.choice(outputs)
-    
+    raise 
     # Print the selected output
     print(selected_output)
 
